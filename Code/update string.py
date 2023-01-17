707d3965948195d672f46607a13eecbd14142cd5
Harvanz <115876072+MohamadIrvanZidni@users.noreply.github.com>
2023-01-18 00:05:14 +0700

    commit

---
+++ Code/update string.py
@@ -0,0 +1,3 @@
+# Mengupdate String
+message = "Hello Wordl!"
+print("Update String : - ", message[:6] + "Python")
---
+++ Code/value of string.py
@@ -0,0 +1,5 @@
+# Mengakses Nilai dalam String
+var1 = "Hello word!"
+var2 = "Python Programming"
+print("var1[0] : ", var1[0]) # Mengambil Karakter Pertama
+print("var[1:5] : ", var2[1:5]) # Karakter 2 s/d ke-5
