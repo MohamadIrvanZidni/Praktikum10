14806a79678b09cbd406eee71c264fe886d52b71
Harvanz <115876072+MohamadIrvanZidni@users.noreply.github.com>
2023-01-18 00:27:01 +0700

    commit

---
+++ Code/Latihan 1.py
@@ -0,0 +1,16 @@
+# Latihan1
+txt = "Hello World"
+# Menghitung jumlah karakter
+print(len(txt))
+# Mengambil karakter terakhir
+print(txt[10])
+# Mengambil karakter index ke-2 sampai index ke-4 (llo)
+print(txt[2:5])
+# Menghilangkan spasi pada text tersebut (HelloWorld)
+print(txt.replace(" ", ""))
+# Mengubah text menjadi huruf besar
+print(txt.upper())
+# Mengubah text menjadi huruf kecil
+print(txt.lower())
+# Mengganti karakter H dengan karakter J
+print(txt.replace("H", "J"))
---
+++ Code/Latihan 2.py
@@ -0,0 +1,4 @@
+# Latihan 2
+umur = 24
+txt = "Hello, nama saua john, dan umur saya adalah {} tahun"
+print(txt.format(umur))
